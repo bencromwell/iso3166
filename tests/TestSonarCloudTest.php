99372c8d7d667f395877a77357e3997bd43bec20
Ben Cromwell <ben@bencromwell.com>
2021-08-20 16:28:09 +0100

    Add test coverage

---
+++ tests/TestSonarCloudTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use Cromwell\ISO3166\TestSonarCloud;
+use PHPUnit\Framework\TestCase;
+
+class TestSonarCloudTest extends TestCase
+{
+    public function testSonarCloudTest(): void
+    {
+        $testSonarCloud = new TestSonarCloud('hello', 'world');
+
+        $this->assertEquals('hello', $testSonarCloud->getHello());
+        //$this->assertEquals('world', $testSonarCloud->getWorld()); // not yet
+    }
+}
